f06f80e1c20a5d996ef4d8604430b407cde33fca
tiarahasnabila <tiarahasnabila@gmail.com>
2024-10-18 13:18:02 +0700

    membuat projek baru empty activity dan menambahkan @preview dan @composable

---
+++ app/src/main/java/com/example/mainactivity/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.mainactivity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mainactivity.ui.theme.MainActivityTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            MainActivityTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicCompose(
+                        tiara = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BasicCompose(tiara: Modifier = Modifier){
+    Column(verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "Mata Kuliah", modifier = tiara,
+            fontSize = 50.sp,
+            color = Color.Red,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(text = "Pemrograman Aplikasi Mobile", modifier = tiara,
+            fontSize = 50.sp,
+            color = Color.Red,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            text = "Nama",)
+
+
+        Image(painter = painterResource(id = R.drawable.kucingnya),
+            contentDescription = null,
+            Modifier.size(250.dp)        )
+
+    }
+}
+
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MainActivityTheme {
+//        Greeting("Android")
+//    }
+//}
